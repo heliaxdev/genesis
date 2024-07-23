669f2506c669b0698a552d90fb4f191d068b0ab3
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-07-23 13:45:23 +0200

    wip

--- scripts/validate-merge.py
+++ scripts/validate-merge.py
@@ -1,14 +1,26 @@
 import glob
 import os
+import subprocess
 
 def get_all_merged_transactions():
     return glob.glob("transactions/*-*.toml")
+
 
 def get_alias():
     alias = os.environ.get("ALIAS")
     if alias is None:
         exit(1)
     return alias
+
+
+def is_valid_template():
+    namada_binaries_path = os.environ.get("NAMADA_PATH", 'namada')
+    res = subprocess.run([namada_binaries_path, "client", "utils", "validate-genesis-templates", "--path", "genesis"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if res.returncode > 0:
+        print(res.stderr)
+        print(res.stdout)
+        exit(1)
+
 
 def main():
     transactions = get_all_merged_transactions()
@@ -20,6 +32,8 @@ def main():
         print("Adding {}...".format(file))
         new_transaction = open(file, "r")
         genesis_transactions.write("{}\n".format(new_transaction.read()))
+
+    is_valid_template()
 
     print("Done.")
 
