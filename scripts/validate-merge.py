f8790d53c3a4b92093a16de69caae2261f447a81
Ian <ikay@lakeheadu.ca>
2024-07-26 11:22:30 -0400

    Update validate-merge.py

--- scripts/validate-merge.py
+++ scripts/validate-merge.py
@@ -27,6 +27,9 @@ def main():
         new_transaction = open(file, "r")
         genesis_transactions.write("{}\n".format(new_transaction.read()))
 
+    # flush write buffer
+    genesis_transactions.close()
+
     is_valid_template()
 
     print("Done.")
